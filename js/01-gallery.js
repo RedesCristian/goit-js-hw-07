d717c68ddf6103867fd60dbe09780a47ce82ad2c
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-04-13 00:16:18 +0300

    ex 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,29 +6,28 @@ function createImagesGallery() {
   for (const image of galleryItems) {
     const listElement = document.createElement("li");
     listElement.classList.add("gallery__item");
-      
+
     const a = document.createElement("a");
     a.classList.add("gallery__link");
     a.setAttribute("href", image.original);
     const img = document.createElement("img");
-      
-    img.classList.add("gallery__image"); 
+
+    img.classList.add("gallery__image");
     img.setAttribute("src", image.preview);
     img.setAttribute("alt", image.description);
-    
+
     list.append(listElement);
     listElement.append(a);
     a.append(img);
-    
-    
+
     a.addEventListener("click", (event) => {
       event.preventDefault();
-      
+
       const instance = basicLightbox.create(
         `<img src="${img.src}" alt="${img.alt}">`
       );
       instance.show();
-      
+
       document.addEventListener("keyup", (event) => {
         if (event.code === "Escape") {
           instance.close();
