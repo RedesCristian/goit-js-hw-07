ee4f4245b2bcaddb83b3bd2c70867c4f302658ff
RedesCristian <149571424+RedesCristian@users.noreply.github.com>
2024-04-13 22:46:15 +0300

    update to large image

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,7 +13,7 @@ function createImagesGallery() {
     const img = document.createElement("img");
 
     img.classList.add("gallery__image");
-    img.setAttribute("src", image.preview);
+    img.setAttribute("src", image.original);
     img.setAttribute("alt", image.description);
 
     list.append(listElement);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,3 +1,34 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-let list = document.querySelector(".gallery");
+
+const list = document.querySelector(".gallery");
+
+function createImagesGallery() {
+  for (const image of galleryItems) {
+    const listElement = document.createElement("li");
+    listElement.classList.add("gallery__item");
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.setAttribute("href", image.original);
+    const img = document.createElement("img");
+
+    img.classList.add("gallery__image");
+    img.setAttribute("src", image.preview);
+    img.setAttribute("alt", image.description);
+
+    list.append(listElement);
+    listElement.append(a);
+    a.append(img);
+
+    a.addEventListener("click", x);
+    function x(event) {
+      event.preventDefault();
+      img.src = image.original;
+    }
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsDelay: 250,
+    captionsData: "alt",
+  });
+}
+createImagesGallery();
